ac9e98d6e145a45415e8b17e8a4d562355c69f68
DIOC2025 <diortega@uamv.edu.ni>
2025-05-15 13:12:21 -0600

    Update Ejercicio#9.py

--- Ejercicio#9.py
+++ Ejercicio#9.py
@@ -5,3 +5,37 @@ y se entrevistará a cinco estudiantes por grupo. Se debe registrar si el estudi
 estable, intermitente o no tiene internet. Al final, mostrar un conteo de cada tipo de acceso por
 carrera y el total general.
 """
+
+print("Encuesta: Acceso a internet en casa.")
+
+#Primero se definen datos que ya conocemos
+carreras = ["Medicina", "Ingenieria en Sistemas", "Aquitectura"]
+gruposCarrera = 3
+estudiantesGrupo = 5
+tiposInternet = ["Estable", "Intermitente", "Nulo"]
+#Aqui se hace un arreglo bidimensional para almacenar las respuestas por cada carrera
+resultados = [
+    #Medicina
+    [0, 0, 0],
+    #Ingenieria en sistemas
+    [0, 0, 0],
+    #Arquitectura
+    [0, 0, 0]
+]
+#Aqui se hace otro arreglo bidimensional que guarde los datos totales
+total = [0, 0, 0]
+
+#Recorremos las carreras y grupos, empezamos el formulario
+for i in range (3):
+    print(f"Carrera: {carreras[i]}")
+    #Recorremos grupos
+    for g in range(3):
+        #Recorremos personas
+        for h in range (5):
+            #Añadimos un while para confirmar que se ingresen los datos necesarios para que el programa funcione como es debido
+            while True:
+                print(f"Tipos de acceso: {tiposInternet}")
+                #Capitalize y strip se utilizan para añadir mayuscula inicial a palabras y para eliminar espacios que puedan afectar 
+                #el reconocimiento de la palabra ingresada
+                tipoAcceso = input("Ingrese su tipo de acceso a internet").capitalize().strip
+                resultados.append = tipoAcceso
